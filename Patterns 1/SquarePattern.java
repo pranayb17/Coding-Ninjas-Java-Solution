f1b1169c3fb43f83d11eb32e1f408ea3dc283bb9
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-05-10 20:43:11 +0530

    Update SquarePattern.java

--- Patterns 1/SquarePattern.java
+++ Patterns 1/SquarePattern.java
@@ -1,3 +1,30 @@
+// Code : Square Pattern
+// Send Feedback
+// Print the following pattern for the given N number of rows.
+// Pattern for N = 4
+// 4444
+// 4444
+// 4444
+// 4444
+// Input format :
+// Integer N (Total no. of rows)
+// Output format :
+// Pattern in N lines
+// Constraints
+// 0 <= N <= 50
+// Sample Input 1:
+// 7
+// Sample Output 1:
+// 7777777
+// 7777777
+// 7777777
+// 7777777
+// 7777777
+// 7777777
+// 7777777
+
+
+
 import java.util.Scanner;
 public class Solution {
 
