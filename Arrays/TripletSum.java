6f877524fed1e6df21207360137bea953e454f40
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-05-12 16:56:53 +0530

    Update TripletSum.java

--- Arrays/TripletSum.java
+++ Arrays/TripletSum.java
@@ -34,6 +34,16 @@
 // 19
 // 9
 // 2 -5 8 -6 0 5 10 11 -3
+// 10
+// Sample Output 2:
+// 0
+// 5
+
+
+//  Explanation for Input 2:
+// Since there doesn't exist any triplet with sum equal to 19 for the first query, we print 0.
+
+// For the second query, we have 5 triplets in total that sum up to 10. They are, (2, 8, 0), (2, 11, -3), (-5, 5, 10), (8, 5, -3) and (-6, 5, 11)
 
 
 public class Solution {
@@ -62,13 +72,3 @@ public class Solution {
            
     }
 }
-// 10
-// Sample Output 2:
-// 0
-// 5
-
-
-//  Explanation for Input 2:
-// Since there doesn't exist any triplet with sum equal to 19 for the first query, we print 0.
-
-// For the second query, we have 5 triplets in total that sum up to 10. They are, (2, 8, 0), (2, 11, -3), (-5, 5, 10), (8, 5, -3) and (-6, 5, 11)
