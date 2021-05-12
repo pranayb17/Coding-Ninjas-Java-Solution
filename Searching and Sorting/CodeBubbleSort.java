262b8d7088e200d3ae934427d197a7879eeda0c4
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-05-12 17:10:45 +0530

    Update CodeBubbleSort.java

--- Searching and Sorting/CodeBubbleSort.java
+++ Searching and Sorting/CodeBubbleSort.java
@@ -34,13 +34,16 @@
 // 1 2 3 4
 
 
+
 public class Solution {  
 
     public static void bubbleSort(int[] arr){
+        
+        int size=arr.length;
     	
-    	for (int i=0;i<arr.length-1;i++) {
+    	for (int i=0;i<size-1;i++) {
     		
-    		for(int j=0;j<arr.length-1-i;j++) {
+    		for(int j=0;j<size-i-1;j++) {
     			
     			
     			if(arr[j]>arr[j+1]) {
@@ -48,8 +51,8 @@ public class Solution {
     				arr[j]=arr[j+1];
     				arr[j+1]=temp;
     				
-    			  }
-    		 }		
-      }	
-   } 
+    			}
+    		}		
+        }	
+    }  
 }
