ac5a2010e41d9387804d9ee6f9fb2fdd55d1173f
Pranay Borkar <78843621+pranayb17@users.noreply.github.com>
2021-06-06 15:50:05 +0530

    Create JaggedArray.java

---
+++ Strings and 2D Arrays/JaggedArray.java
@@ -0,0 +1,22 @@
+public class JaggedArrays {
+
+	public static void main(String[] args) {
+		int[][] arr = new int[4][0];
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = new int[i+2];
+		}
+		printJaggedArray(arr);
+	}
+
+	private static void printJaggedArray(int[][] arr) {
+		
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0;j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
